1525884b22b0e7c23ca5ad45e300b4e8ef857eac
Prateek Kumar Oraon <b170078@nitsikkim.ac.in>
2020-11-15 02:27:36 +0530

    [DOCUMENTATION] filters.py

--- filters.py
+++ filters.py
@@ -4,11 +4,23 @@ import mimetypes
 
 
 def date_time_format(date_str):
+    """Constructs easy human readable date format from.
+
+    Returns:
+        str: Date.
+    """
+
     dt = arrow.get(date_str)
     return dt.humanize()
 
 
 def file_type(key):
+    """Gets file extension.
+
+    Returns:
+        str: File extension.
+    """
+    
     file_info = os.path.splitext(key)
     file_extension = file_info[1]
 
